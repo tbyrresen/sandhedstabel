b214abd2dd9eb2e73711689d99a0a55ef18420b2
Tobias Byrresen <tbyrresen@gmail.com>
2020-06-01 21:08:29 +0200

    Added componentDidUpdate to TruthTableBuilder and implemented error messages from Tokenizer class

--- src/containers/TruthTableBuilder.js
+++ src/containers/TruthTableBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
+import Tokenizer from '../utils/tokenizer';
 
 class TruthTableBuilder extends Component {
     constructor(props) {
@@ -7,7 +8,27 @@ class TruthTableBuilder extends Component {
         this.state = {
             expression: "",
             truthValueFormat: "T/F",
+            valid: true,
+            error: null
         }
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        if (prevState.expression !== this.state.expression) {
+            try {
+                this.setState({
+                    valid: true,
+                    error: null
+                })
+                const tokens = Tokenizer.tokenize(this.state.expression);
+            }
+            catch (error) {
+                this.setState({
+                    valid: false,
+                    error: error
+                })
+            }
+        }        
     }
 
     expressionChangeHandler = (event) => {
@@ -35,16 +56,14 @@ class TruthTableBuilder extends Component {
                             name="truthValueFormat"
                             id="trueFalseFormat"
                             onClick={this.truthValueFormatChangeHandler}
-                            defaultChecked 
-                        />
+                            defaultChecked />
                         <Form.Check 
                             type="radio"
                             label="1/0"
                             value="1/0"
                             name="truthValueFormat"
                             id="binaryFormat"
-                            onClick={this.truthValueFormatChangeHandler}
-                        />
+                            onClick={this.truthValueFormatChangeHandler} />
                     </Form.Group>
                     <Form.Group>                  
                         <Form.Text className="text-muted">
@@ -55,9 +74,10 @@ class TruthTableBuilder extends Component {
                             type="text"
                             placeholder="Dit udtryk her"
                             onChange={this.expressionChangeHandler}
-                            onKeyPress={(event) => { event.key === 'Enter' && event.preventDefault() }}
-                        />
-                        <small>Eksempel på udtryk: a AND (b OR C)</small>                   
+                            onKeyPress={(event) => { event.key === 'Enter' && event.preventDefault() }} />
+                        <small>Eksempel på udtryk: a AND (b OR C)</small>      
+                        {this.state.valid ? <p className="text-success">TODO</p> :
+                        <p className="text-danger">{this.state.error.message}</p>}             
                     </Form.Group>
                 </Form>
             </div>
